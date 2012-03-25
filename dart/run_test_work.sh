9f8f63ff2f93d6a2b91e64af2f4a3f9dc4da7551
Filip Hracek <filiph@google.com>
2012-03-25 23:59:41 +0200

    Updated run_test_work.sh from run_test.sh.

--- dart/run_test_work.sh
+++ dart/run_test_work.sh
@@ -1,2 +1,3 @@
-/Applications/dart/dart-sdk/bin/dart /Users/filiph/dev/egamebook/dart/egb_builder.dart /Users/filiph/dev/egamebook/dart/samples/unit-testing.markdown
-/Applications/dart/dart-sdk/bin/dart /Users/filiph/dev/egamebook/dart/egb_interface_cmdline.dart
+/Applications/dart/dart-sdk/bin/dart /Users/filiph/dev/egamebook/dart/egb_builder.dart /Users/filiph/dev/egamebook/dart/library/tis_001.egb
+#/Applications/dart/dart-sdk/bin/frogc /Users/filiph/dev/egamebook/dart/library/tis_001.html.dart
+/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts /Users/filiph/dev/egamebook/dart/library/tis_001.cmdline.dart
