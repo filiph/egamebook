17696bdba8d1fb21cf8e8d801513b10e754e67ca
Filip Hracek <filiph@google.com>
2012-07-12 01:36:29 +0200

    get rid of old TODOs

--- dart/egb_builder.dart
+++ dart/egb_builder.dart
@@ -2,15 +2,6 @@
 
 #import('dart:io');
 
-// TODO: convert all non-ASCII characters to HTML entities (&iacute; done, but what about czech chars?)
-// TODO: generate a Map<String,int> so that goto("someRoom"); works inside <script> blocks
-// TODO: another region is "initialize" - to init vars["smthing"] so that they're there no matter where you open the gamebook.
-// XXX: consolidate all the blocks automatically - classes go to class, functions go to library, everything else goes to init.
-// TODO: make it easy to package libraries and import them - importLibrary("FGBE", version:1.0);
-// XXX: use markdown block_parser.dart for parsing the text (just line parser is fine)
-// XXX: make sure v_something -> vars["something"] doesn't break quotes (like in echo("something v_something");)
-
-
 class EgbFormatException implements Exception {
   String msg;
   EgbFormatException([String this.msg]) {
