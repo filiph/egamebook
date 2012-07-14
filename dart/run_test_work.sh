716cb16dd47ddc8f520565312d55159707c82127
Filip Hracek <filiph@google.com>
2012-07-14 15:22:53 +0200

    Deleted unnecessary file.

--- dart/run_test_work.sh
+++
@@ -1,3 +0,0 @@
-/Applications/dart/dart-sdk/bin/dart /Users/filiph/dev/egamebook/dart/build.dart /Users/filiph/dev/egamebook/dart/library/tis_001.egb
-/Applications/dart/dart-sdk/bin/dart2js -c -o/Users/filiph/dev/egamebook/dart/library/tis_001.html.dart.js /Users/filiph/dev/egamebook/dart/library/tis_001.html.dart
-/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts /Users/filiph/dev/egamebook/dart/library/tis_001.cmdline.dart
