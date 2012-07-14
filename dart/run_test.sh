5c94adf4cf73e16d02e4b045b8083b2ea86cb71b
Filip Hracek <filiph@google.com>
2012-07-14 15:12:10 +0200

    run_test now works independently on file structure

--- dart/run_test.sh
+++ dart/run_test.sh
@@ -1,3 +1,4 @@
-/Applications/dart/dart-sdk/bin/dart /Users/filiph/Programs/egamebook/dart/egb_builder.dart /Users/filiph/Programs/egamebook/dart/library/tis_001.egb
-/Applications/dart/dart-sdk/bin/dart2js -c -o/Users/filiph/Programs/egamebook/dart/library/tis_001.html.dart.js /Users/filiph/Programs/egamebook/dart/library/tis_001.html.dart
-/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts /Users/filiph/Programs/egamebook/dart/library/tis_001.cmdline.dart
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+/Applications/dart/dart-sdk/bin/dart $DIR/build.dart $DIR/library/tis_001.egb
+/Applications/dart/dart-sdk/bin/dart2js -c -o$DIR/library/tis_001.html.dart.js $DIR/library/tis_001.html.dart
+/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/library/tis_001.cmdline.dart
