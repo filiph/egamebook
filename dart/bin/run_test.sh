6d4ba950c36eb752c32a22ca19a00485ccb7e74b
filiph <filip.hracek@gmail.com>
2012-10-21 22:44:15 +0200

    Adding HTML file to /example/test. Right now, JavaScript is not compiling properly, though.

--- dart/bin/run_test.sh
+++ dart/bin/run_test.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 /Applications/dart/dart-sdk/bin/dart $DIR/build.dart $DIR/../example/test/test.egb
-#/Applications/dart/dart-sdk/bin/dart2js -c -o$DIR/../example/test/test.html.dart.js $DIR/../example/test/test.html.dart
+/Applications/dart/dart-sdk/bin/dart2js -c -o$DIR/../example/test/test.html.dart.js $DIR/../example/test/test.html.dart
 /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../example/test/test.cmdline.dart
