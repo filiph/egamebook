0fec4995d39ba01298b0b635a93f5a55a6d43731
filiph <filip.hracek@gmail.com>
2012-10-29 22:18:08 +0100

    Temporarily omit --checked in dart2js compiling. Solves the bug (?) with set method not set in JavaScript

--- dart/bin/run_test.sh
+++ dart/bin/run_test.sh
@@ -1,4 +1,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 /Applications/dart/dart-sdk/bin/dart $DIR/build.dart $DIR/../example/test/test.egb
-/Applications/dart/dart-sdk/bin/dart2js -c -o$DIR/../example/test/test.html.dart.js $DIR/../example/test/test.html.dart
-/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../example/test/test.cmdline.dart
+#/Applications/dart/dart-sdk/bin/dart2js --checked -v --package-root=$DIR/../packages --out=$DIR/../example/test/test.html.dart.js $DIR/../example/test/test.html.dart
+/Applications/dart/dart-sdk/bin/dart2js -v --package-root=$DIR/../packages --out=$DIR/../example/test/test.html.dart.js $DIR/../example/test/test.html.dart
+
+#/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../example/test/test.cmdline.dart
