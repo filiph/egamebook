00d76b28e53a78cb5e9941cc167e232bb74f2bed
filiph <filip.hracek@gmail.com>
2013-01-31 22:56:01 +0100

    Add the abstract class Saveable

---
+++ dart/lib/src/persistence/saveable.dart
@@ -0,0 +1,17 @@
+library egb_saveable;
+
+abstract class Saveable {
+  /**
+   * When serializing a custom class and when it's Saveable, the Scripter
+   * will call this method to get a Map describing this instance. The class
+   * should be able to recreate its state from this Map.
+   */
+  Map<String,dynamic> toMap();
+  
+  /**
+   * Update the instance from a Map. Typically, the Map is a one that was
+   * earlier set by the [toMap()] method and that was saved by the Runner
+   * to a Storage.
+   */
+  void updateFromMap(Map<String,dynamic> map);
+}
