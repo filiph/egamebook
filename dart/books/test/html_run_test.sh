eed710a6d465620f405d6c61c408bc2009bee194
Filip Hracek <filiph@google.com>
2013-02-05 21:18:07 +0100

    Compile only if builder succeeds, compile with -c flag

--- dart/books/test/html_run_test.sh
+++ dart/books/test/html_run_test.sh
@@ -1,4 +1,3 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-/Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/test.egb
-/Applications/dart/dart-sdk/bin/dart2js -v --package-root=$DIR/../../packages --out=$DIR/test.html.dart.js $DIR/test.html.dart
+/Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/test.egb && /Applications/dart/dart-sdk/bin/dart2js -v -c --package-root=$DIR/../../packages --out=$DIR/test.html.dart.js $DIR/test.html.dart
 open $DIR/test.html
