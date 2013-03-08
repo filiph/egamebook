9bd747c00cd02d68f003d1964e82c7f3a458884f
Filip Hracek <filiph@google.com>
2013-03-08 13:47:17 +0100

    Stop minifying js source

--- dart/books/test/html_run_test.sh
+++ dart/books/test/html_run_test.sh
@@ -1,3 +1,3 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-/Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/test.egb && /Applications/dart/dart-sdk/bin/dart2js -v --minify -c --package-root=$DIR/../../packages --out=$DIR/test.html.dart.js $DIR/test.html.dart
+/Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/test.egb && /Applications/dart/dart-sdk/bin/dart2js -v -c --package-root=$DIR/../../packages --out=$DIR/test.html.dart.js $DIR/test.html.dart
 open $DIR/test.html
