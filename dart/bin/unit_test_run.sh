f29575d8d27fca45edbb59965e62a3d0cd5c4ee2
filiph <filip.hracek@gmail.com>
2013-03-15 10:06:07 +0100

    Start unit testing the libraries using the main unit testing script

--- dart/bin/unit_test_run.sh
+++ dart/bin/unit_test_run.sh
@@ -1,2 +1,11 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../test/builder_test.dart && /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../test/scripter_test.dart
+if [ $? -gt 0 ]; then
+  exit
+fi
+for f in $( ls $DIR/../books/libraries/*_test.dart ); do
+  /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $f
+  if [ $? -gt 0 ]; then
+    exit
+  fi
+done
