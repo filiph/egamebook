1980cff43f0ec196fb3de86526f66d5859edc41f
filiph <filip.hracek@gmail.com>
2013-04-01 01:08:41 +0200

    Make sure test.egb is UTF-8

--- dart/books/test/html_run_test.sh
+++ dart/books/test/html_run_test.sh
@@ -1,3 +1,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 /Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/test.egb && /Applications/dart/dart-sdk/bin/dart2js -v -c --package-root=$DIR/../../packages --out=$DIR/test.html.dart.js $DIR/test.html.dart
-open $DIR/test.html
+cd $DIR
+open http://0.0.0.0:8000/test.html
+python -m SimpleHTTPServer
+cd -
