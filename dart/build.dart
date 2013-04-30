ded22b3a537974d8dce705444fef0d992b640162
Filip Hracek <filiph@google.com>
2013-04-30 14:28:34 +0200

    Comment out the build.dart main() body, it hangs anyway

--- dart/build.dart
+++ dart/build.dart
@@ -2,17 +2,17 @@ library builder;
 import 'dart:io';
 
 main() {
-  print("Running unit tests...");
-  Process.run("./bin/unit_test_run.sh", [])
-  .then((result) {
-    if (result.exitCode == 0) {
-      print("  success.");
-    } else {
-      print("  FAILED!\n");
-      print(result.stdout);
-      exitCode = result.exitCode;
-    }
-  }, onError: (e) {
-    print(e);
-  });
+//  print("Running unit tests...");
+//  Process.run("./bin/unit_test_run.sh", [])
+//  .then((result) {
+//    if (result.exitCode == 0) {
+//      print("  success.");
+//    } else {
+//      print("  FAILED!\n");
+//      print(result.stdout);
+//      exitCode = result.exitCode;
+//    }
+//  }, onError: (e) {
+//    print(e);
+//  });
 }
