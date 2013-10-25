fc7f14e0d4a9e19dde8fdb2504dd0d4affc914dd
Filip Hracek <filiph@google.com>
2013-10-25 19:16:08 +0200

    Add bodega analyzer test for easier sanity checks

---
+++ dart/bin/build_bodega_test.sh
@@ -0,0 +1,19 @@
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+# Build bodega.
+cd $DIR/../books/bodega
+./build.sh
+# Run analyzer
+/Applications/dart/dart-sdk/bin/dartanalyzer ./bodega.html.dart
+if [ $? -eq 2 ]; then
+  echo "ERROR WITH BODEGA BUILD!"
+  exit 1
+elif [ $? -eq 1 ]; then
+  echo "There were some warnings, but nothing fatal."
+  exit 0
+elif [ $? -eq 0 ]; then
+  echo "No problems with build."
+  exit 0
+else
+  echo "Invalid exit code from dartanalyzer!"
+  exit 1
+fi
