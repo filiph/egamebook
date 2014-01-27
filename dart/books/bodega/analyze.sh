14833af7486936d466f1705afda38a8178d1ab37
filiph <filip.hracek@gmail.com>
2014-01-27 19:44:37 +0100

    Report Builder errors with watch.sh and analyze.sh

--- dart/books/bodega/analyze.sh
+++ dart/books/bodega/analyze.sh
@@ -1,12 +1,22 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Build bodega.
 $DIR/build.sh
+if [ $? -eq 255 ]; then
+  # Error code 255 is Dart reporting a runtime error.
+  # https://api.dartlang.org/docs/channels/stable/latest/dart_io.html#exit
+  echo "ERROR WITH BODEGA BUILD! (in Builder)"
+  if [ $# -gt 0 ]; then
+    osascript -e 'tell app "Terminal" to display alert "Error when building egamebook!"'
+  fi
+  exit 1
+fi
+echo "Hi"
 # Run analyzer
 # You can change to bodega.html.dart for longer check in case there are any strange interferences.
 # But checking just bodega.dart should suffice.
 /Applications/dart/dart-sdk/bin/dartanalyzer --machine $DIR/bodega.dart
 if [ $? -eq 2 ]; then
-  echo "ERROR WITH BODEGA BUILD!"
+  echo "ERROR WITH BODEGA BUILD! (in Analyzer)"
   if [ $# -gt 0 ]; then
     osascript -e 'tell app "Terminal" to display alert "Error when building egamebook!"'
   fi
--- dart/books/bodega/build.sh
+++ dart/books/bodega/build.sh
@@ -1,2 +1,3 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-$DIR/copy_from_drive.sh && /Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/bodega.egb
+$DIR/copy_from_drive.sh && \
+  /Applications/dart/dart-sdk/bin/dart $DIR/../../bin/build.dart $DIR/bodega.egb
