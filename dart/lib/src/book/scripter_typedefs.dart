ca7ffde98acc4bc7682944d19595d6d236244256
filiph <filip.hracek@gmail.com>
2014-07-23 22:22:04 +0200

    Add typedefs for scripter top-level functions

---
+++ dart/lib/src/book/scripter_typedefs.dart
@@ -0,0 +1,23 @@
+library scripter_typedefs;
+import 'package:egamebook/src/shared/user_interaction.dart';
+import 'package:egamebook/src/shared/form.dart';
+
+/**
+ * A compendium of typedefs to [EgbScripter]'s top-level functions such as
+ * [:echo:] and [:goto:].
+ */
+
+/// Corresponds to [EgbScripter.goto].
+typedef void GotoFunction(String pageName);
+
+/// Corresponds to [EgbScripter.echo].
+typedef void EchoFunction(String str);
+
+/// Corresponds to [EgbScripter.choice].
+typedef EgbChoice ChoiceFunction(String string, {String goto, Function script, 
+    String submenu, bool deferToEndOfPage, bool deferToChoiceList});
+
+/// Corresponds to [EgbScripter.showForm].
+typedef void ShowFormFunction(Form form);
+
+// TODO: add nextScript(), repeatBlock() _only_ if necessary
