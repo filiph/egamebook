e18ac8a06a5f4b56db2e1af06e68f7a33fb3e6dc
Jana Moudrá <jana.moudra@gmail.com>
2015-05-03 12:48:25 +0200

    Removed TODOs

--- dart/bin/egamebook.dart
+++ dart/bin/egamebook.dart
@@ -3,8 +3,6 @@ import 'package:args/command_runner.dart';
 import 'package:egamebook/command.dart';
 
 /// TODO use async/await
-/// TODO do we want to use symlinks when copying project files?
-/// TODO do we want to run dartanalyzer after build?
 ///
 /// Runs egamebook script with [args] which expects commands in form:
 ///   create <path>
