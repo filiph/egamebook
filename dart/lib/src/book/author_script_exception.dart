4a130e76f6ebba12cdbffc46dbd147a113c7fa65
Jana Moudrá <jana.moudra@gmail.com>
2015-06-11 14:02:44 +0200

    Added comments for AuthorScriptException

--- dart/lib/src/book/author_script_exception.dart
+++ dart/lib/src/book/author_script_exception.dart
@@ -5,11 +5,20 @@ library author_script_exception;
  * are caught and exported.
  */
 class AuthorScriptException implements Exception {
+  /// Message describing the exception.
   final String message;
+  /// Page name that is being read.
   final String pagename;
+  /// The position in the page's blocks list.
   final int blockIndex;
+  /// Creates new [AuthorScriptException] with error [message]. [pagename] and
+  /// [blockIndex] are set to null.
   const AuthorScriptException(this.message) : pagename = null, blockIndex = null;
+  /// Creates new [AuthorScriptException] with error [message], actual [pagename],
+  /// and position in the page's blocks list stored in [blockIndex].
   const AuthorScriptException.withPageName(this.message, this.pagename, this.blockIndex);
+  /// Returns text describing [AuthorScriptException] with its [message], [pagename]
+  /// and [blockIndex] where the exception appeared.
   String toString() => "AuthorScriptException at page '$pagename', "
       "block #$blockIndex: $message";
 }
