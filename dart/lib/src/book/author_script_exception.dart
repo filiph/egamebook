fc704145b6829236479abfd23c21f99ce5b60408
Jana Moudrá <jana.moudra@gmail.com>
2015-06-11 14:08:29 +0200

    Fixed naming of pageName attribute in AuthorScriptException

--- dart/lib/src/book/author_script_exception.dart
+++ dart/lib/src/book/author_script_exception.dart
@@ -8,17 +8,17 @@ class AuthorScriptException implements Exception {
   /// Message describing the exception.
   final String message;
   /// Page name that is being read.
-  final String pagename;
+  final String pageName;
   /// The position in the page's blocks list.
   final int blockIndex;
-  /// Creates new [AuthorScriptException] with error [message]. [pagename] and
+  /// Creates new [AuthorScriptException] with error [message]. [pageName] and
   /// [blockIndex] are set to null.
-  const AuthorScriptException(this.message) : pagename = null, blockIndex = null;
-  /// Creates new [AuthorScriptException] with error [message], actual [pagename],
+  const AuthorScriptException(this.message) : pageName = null, blockIndex = null;
+  /// Creates new [AuthorScriptException] with error [message], actual [pageName],
   /// and position in the page's blocks list stored in [blockIndex].
-  const AuthorScriptException.withPageName(this.message, this.pagename, this.blockIndex);
-  /// Returns text describing [AuthorScriptException] with its [message], [pagename]
+  const AuthorScriptException.withPageName(this.message, this.pageName, this.blockIndex);
+  /// Returns text describing [AuthorScriptException] with its [message], [pageName]
   /// and [blockIndex] where the exception appeared.
-  String toString() => "AuthorScriptException at page '$pagename', "
+  String toString() => "AuthorScriptException at page '$pageName', "
       "block #$blockIndex: $message";
 }
