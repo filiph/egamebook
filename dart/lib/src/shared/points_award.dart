a97b93df5aa5649c1ce4cc913ba41e09a1b9a828
Jana Moudrá <jana.moudra@gmail.com>
2015-06-21 16:26:33 +0200

    Updated documentation for points award.

--- dart/lib/src/shared/points_award.dart
+++ dart/lib/src/shared/points_award.dart
@@ -3,7 +3,7 @@ library points_award;
 import 'message.dart';
 
 /**
- * One PointAward is a bundle of points awarded for something concrete
+ * One PointsAward is a bundle of points awarded for something concrete
  * to the player. This is mostly done by something like
  * [:points.add(5, "bravery"):] and will be presented as "+5 points for
  * bravery".
@@ -13,11 +13,16 @@ class PointsAward {
   final int addition;
   /// The resulting sum of points after this award.
   final int result;
-  /// The (optional) justification for the award.
+  /// The (optional) justification message for the award.
   final String justification;
 
+  /// Creates new PointsAward with number of points awarded in [addition],
+  /// resulting sum of points in [result] and optional [justification]
+  /// message why the points were awarded.
   PointsAward(this.addition, this.result, [this.justification]);
 
+  /// Returns string representation of PointsAward in format of score
+  /// with points in [addition] and optional [justification] message.
   String toString() {
     if (justification != null) {
       return "Score +$addition for $justification.";
@@ -26,6 +31,7 @@ class PointsAward {
     }
   }
 
+  /// Returns PointsAward as an [EgbMessage].
   EgbMessage toMessage() {
     var message = new EgbMessage(EgbMessage.POINTS_AWARD);
     message.listContent = [addition, result];
@@ -33,6 +39,7 @@ class PointsAward {
     return message;
   }
 
+  /// Creates and returns new PointsAward from [EgbMessage].
   factory PointsAward.fromMessage(EgbMessage message) {
     int addition = message.listContent[0];
     int result = message.listContent[1];
