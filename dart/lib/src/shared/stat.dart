051a29f1c596c1403de0823eddd1bfa964ac5ccb
Jana Moudrá <jana.moudra@gmail.com>
2015-06-25 17:47:24 +0200

    Updated doc comments for stats.

--- dart/lib/src/shared/stat.dart
+++ dart/lib/src/shared/stat.dart
@@ -31,8 +31,10 @@ abstract class StatBase {
   /// points), but most often it is something else (e.g. "2:12am", "$32", ...).
   String get string;
 
+  /// Returns string representation of StatBase - a [string] attribute.
   String toString() => string;
 
+  /// Default stat color.
   static const String DEFAULT_COLOR = "#CCCCCC";
 }
 
@@ -41,25 +43,37 @@ abstract class StatBase {
  * stays the same.) This is sent from [EgbScripter] to [EgbInterface].
  */
 class StatUpdate {
+  /// Creates new StatUpdate with [show] and short [string].
   StatUpdate(this.show, this.string);
+  /// Signifies whether to show the [Stat] in the presenter or not.
   final bool show;
+  /// The current [String] value of the [Stat].
   final String string;
 
+  /// Returns a Map representation of StatUpdate with its [show] and [string]
+  /// attributes included.
   Map<String, Object> toMap() => {
     "show": show,
     "string": string
   };
+  /// Creates new StatUpdate from a given [map].
   StatUpdate.fromMap(Map<String, Object> map)
       : show = map["show"],
         string = map["string"];
 }
 
+/// Class StatUpdateCollection wraps a collection of stat updates.
 class StatUpdateCollection {
+  /// Map of stat updates.
   final Map<String, StatUpdate> _updates = new Map<String, StatUpdate>();
+  /// Creates a new StatUpdateCollection.
   StatUpdateCollection();
+
+  /// Adds new [StatUpdate] into a stat updates Map.
   void add(String name, StatUpdate update) {
     _updates[name] = update;
   }
+  /// Returns a Map representation of stat updates.
   Map<String, Object> toMap() {
     Map<String, Object> map = new Map<String, Object>();
     _updates.forEach((String name, StatUpdate update) {
@@ -67,20 +81,24 @@ class StatUpdateCollection {
     });
     return map;
   }
+  /// Creates a new StatUpdateCollection from a given [map].
   StatUpdateCollection.fromMap(Map<String, Object> map) {
     map.forEach((String name, Object o) {
       _updates[name] = new StatUpdate.fromMap(o as Map<String, Object>);
     });
   }
 
+  /// Applies a [function] for every stat update.
   void forEach(_StatUpdateCollectionForEachCallback function) {
     _updates.forEach(function);
   }
 }
 
+/// Typedef for stat update callback called on every stat update.
 typedef void _StatUpdateCollectionForEachCallback(String name,
                                                   StatUpdate update);
 
+/// Class UIStat represent one stat shown in UI.
 class UIStat implements StatBase {
   String name;
   String description;
@@ -90,10 +108,15 @@ class UIStat implements StatBase {
   bool notifyOnChange = true; // TODO: implement
   String string;
 
-  UIStat(this.name, this.description, this.color, this.priority, this.show, this.notifyOnChange, this.string);
+  /// Creates new UIStat with [name], [description], [color], [priority],
+  /// if should be visible in [show], if notify on change in [notifyOnChange]
+  /// and short [string].
+  UIStat(this.name, this.description, this.color, this.priority, this.show,
+      this.notifyOnChange, this.string);
 
-  /// Gets the Map from the [STAT_UPDATE] EgbMessage (as generated with
-  /// [Stat._toMessageChangedOnly] and updates the [_stats] from it.
+  /// Gets the Map from [EgbMessage.mapContent] of the received
+  /// [EgbMessage.UPDATE_STATS] message transferred to StatUpdateCollection
+  /// [updates] via [StatUpdateCollection.fromMap] and updates the stats from it.
   ///
   /// Returns the list of the changed stats.
   static List<UIStat> updateStatsList(List<UIStat> statsList,
@@ -108,7 +131,9 @@ class UIStat implements StatBase {
     return changedStats;
   }
 
-  /// Creates a list of [UIStat] objects from an EgbMessage of type [STATS_SET].
+  /// Creates and returns a list of [UIStat] objects from an EgbMessage
+  /// of type [EgbMessage.SET_STATS].
+  ///
   /// The list is sorted by [priority].
   static List<UIStat> overwriteStatsListFromDataStructure(List<Map<String,
       Object>> list) {
@@ -152,9 +177,12 @@ class Stat<T> implements StatBase, Saveable {
   /// The higher the priority, the more prominently (top-side) the Stat will
   /// be shown.
   final int priority;
-  /// The current value. It can be a real number, but when showed, it will
-  /// always be rounded and showed as an integer.
+  /// Getter for the current value. It can be a real number, but when showed,
+  /// it will always be rounded and showed as an integer.
   T get value => _value;
+  /// Setter for the current value. The value is set only if is it different
+  /// from the previous value. In that case the [changed] and [someChanged]
+  /// attributes are set to [:true:].
   set value(T val) {
     if (_value != val) {
       _value = val;
@@ -164,9 +192,12 @@ class Stat<T> implements StatBase, Saveable {
   }
   T _value;
 
-  /// Signifies whether to show the Stat in the presenter or not. Sometimes,
-  /// it's useful to hide a Stat from player before he can use it.
+  /// Getter signifies whether to show the Stat in the presenter or not.
+  /// Sometimes, it's useful to hide a Stat from player before he can use it.
   bool get show => _show;
+  /// Setter if show the Stat in the presenter or not. The value is set only if
+  /// is it different from the previous value. In that case the [changed] and
+  /// [someChanged] attributes are set to [:true:].
   set show(bool value) {
     if (_show != value) {
       _show = value;
@@ -181,6 +212,13 @@ class Stat<T> implements StatBase, Saveable {
   /// the change (e.g. by blinking once).
   bool notifyOnChange = true; // TODO: implement
 
+  /// Creates new Stat with its [name], labda for converting the value to string
+  /// [valueToString] and optional attributes [description], [color], [priority],
+  /// [initialValue] and [show].
+  ///
+  /// If the [Stat] with the given [name] is already contained in internal
+  /// Map of stats, this stat is updated with new value of [initialValue] and
+  /// [show]. Other attributes remain same.
   factory Stat(String name, ValueToStringLambda valueToString, {String
       description, String color: StatBase.DEFAULT_COLOR, int priority: 0, num
       initialValue: 0, bool show: true}) {
@@ -201,6 +239,8 @@ class Stat<T> implements StatBase, Saveable {
     return stat;
   }
 
+  /// Creates new Stat with [name], [description], [valueToString], [color] and
+  /// [priority].
   Stat._internal(this.name, this.description, this.valueToString, this.color, this.priority);
 
   /// Signifies if the [value] (or [show] state) has changed since last time.
@@ -208,17 +248,22 @@ class Stat<T> implements StatBase, Saveable {
   /// the representation of the Stat.
   bool changed = false;
 
+  /// Returns String representation of the value.
   String get string => valueToString(value);
 
   /// True if one of the stats got changed.
   static bool someChanged = false;
 
   static final Map<String, Stat> _stats = new Map<String, Stat>();
+  /// Returns Set of all [Stat] values.
   static Set<Stat> get all => _stats.values;
 
   /**
    * Take all Stats, find updated ones, and send only those, in the form of
    * [StatUpdateCollection].
+   *
+   * The [changed] and [someChanged] attributes are also resetted to default
+   * value of [:false:].
    */
   static StatUpdateCollection createUpdates() {
     StatUpdateCollection updates = new StatUpdateCollection();
@@ -251,8 +296,12 @@ class Stat<T> implements StatBase, Saveable {
     return list;
   }
 
+  /// Class name.
   String className = "Stat";
 
+  /// Returns a Map representation of actual Stat with its attributes [name],
+  /// [value] and [show].
+  ///
   /// The [Saveable.toMap] function.
   Map<String, Object> toMap() => {
       "name": this.name,
@@ -260,6 +309,7 @@ class Stat<T> implements StatBase, Saveable {
       "show": this.show
   };
 
+  /// Updates actual Stat with values from [map].
   void updateFromMap(Map<String, Object> map) {
     assert(map["name"] == this.name);
     this.value = map["value"];
@@ -267,4 +317,5 @@ class Stat<T> implements StatBase, Saveable {
   }
 }
 
+/// Typedef value to String.
 typedef String ValueToStringLambda(Object o);
