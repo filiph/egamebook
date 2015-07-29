5bb0c3e44f4a91c9f889205f9c005504b878a85f
Jana Moudrá <jana.moudra@gmail.com>
2015-07-29 21:49:55 +0200

    Added comments for points counter.

--- dart/lib/src/book/points_counter.dart
+++ dart/lib/src/book/points_counter.dart
@@ -5,40 +5,52 @@ part of egb_scripter;
  * of this class.
  */
 class PointsCounter implements Saveable {
+  /// Actual sum of points.
   int _points = 0;
-  /// The current sum of points.
+  /// Getter returns the current sum of points.
   int get sum => _points;
+  /// Queue of points to be awarded.
   Queue<PointsAward> pointsAwards;
-  
-  // TODO: in order to not break possible arithmetics inside scripts
-  // the PointsCounter should look like it's adding the points even though
-  // it's not.
+
+  /// Number of embargoed points.
+  /// In order to not break possible arithmetics inside scripts the PointsCounter
+  /// should look like it's adding the points even though it's not.
+  /// TODO
   int _embargoedPoints;
-  
+
+  /// Creates new PointsCounter.
   PointsCounter() {
     pointsAwards = new Queue<PointsAward>();
   }
-  
+
+  /// Adds [value] to actual sum of points and adds new [PointsAward] into
+  /// [pointsAwards]. It is also possible to include [justification] message.
   void add(int value, [String justification]) {
     if (!_pointsEmbargo) {
       _points += value;
       pointsAwards.add(new PointsAward(value, _points, justification));
     }
   }
-  
+
+  /// Adds [value] to actual sum of points and adds new [PointsAward] into
+  /// [pointsAwards].
   PointsCounter operator +(int value) {
     add(value);
     return this;
   }
-  
+
+  /// Class name.
   String className = "PointsCounter";
+  /// Returns Map representation of PointsCounter.
   Map toMap() => {"points": _points};
-  
+
+  /// Updates current PointsCounter from a given [map] and clears [pointsAwards].
   void updateFromMap(Map map) {
     _points = map["points"];
     pointsAwards.clear();
   }
 
+  /// Clears whole actual PointsCounter.
   void clear() {
     _points = 0;
     pointsAwards.clear();
