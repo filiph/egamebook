a48662f8ef07cce358c25c3e261ad1a662f6b4d7
Jana Moudrá <jana.moudra@gmail.com>
2015-08-17 21:37:28 +0200

    Updated doc comments for scripter page.

--- dart/lib/src/book/scripter_page.dart
+++ dart/lib/src/book/scripter_page.dart
@@ -11,11 +11,13 @@ class EgbScripterPage extends EgbPage {
 
   /// Number of times this page has been visited by player.
   int visitCount = 0;
-  /// Whether or not this page has been visited by player.
+  /// Getter for whether or not this page has been visited by player.
   bool get visited => visitCount > 0;
 
   /**
-   * Default constructor only takes blocks List, and optionally page options.
+   * Creates new EgbScripterPage and takes only blocks List, and optionally
+   * page options. Page options are [visitOnce] and [showOnce].
+   *
    * Name is copied from Map key when added to [EgbScripterPageMap].
    */
   EgbScripterPage(
@@ -25,24 +27,31 @@ class EgbScripterPage extends EgbPage {
 }
 
 /**
- * Used to store information about an exact point in the book -- page and
+ * Class used to store information about an exact point in the book -- page and
  * block. 
  */
 class EgbScripterBlockPointer {
+  /// Scripter page.
   final EgbScripterPage page;
+  /// Block index.
   final int blockIndex;
-    
+
+  /// Creates new EgbScripterBlockPointer with [page] and [blockIndex].
   EgbScripterBlockPointer(this.page, this.blockIndex); 
 }
 
 /**
  * [EgbScripterPageMap] is the container for the whole of the text and logic
  * content of each book.
+ *
+ * It contains a Map of [EgbScripterPage]s which are accessible through
+ * their names.
  */
 class EgbScripterPageMap {
-  /// A map of page name -> page object.
+  /// A Map of page name -> page object.
   final Map<String, EgbScripterPage> pages = new Map<String, EgbScripterPage>();
 
+  /// Creates new EgbScripterPageMap.
   EgbScripterPageMap();
 
   /// Returns page of exactly the name [key].
@@ -50,7 +59,7 @@ class EgbScripterPageMap {
 
   /**
    * Returns page with name [name]. If [currentGroupName] is given, then 
-   * the function will first search for key in the format [:groupName: name:].
+   * the function will first search for key in a format [:groupName: name:].
    *
    * Returns [:null:] if there is no page of any compatible name.
    */
@@ -65,10 +74,12 @@ class EgbScripterPageMap {
     }
   }
   
-  /// Returns current group name string
+  /// Returns current group name string in a format [:groupName: name:].
   String _getCurrentGroupName(String name, String currentGroupName) 
       => "$currentGroupName: $name";
 
+  /// Saves EgbScripterPage [newPage] on [key] to the Map of [pages].
+  /// Also sets a [key] as a name for the given page.
   operator []=(String key, EgbScripterPage newPage) {
     pages[key] = newPage;
     // Copy the "key" to the name of the page. This is here so that we don't
@@ -76,6 +87,7 @@ class EgbScripterPageMap {
     newPage.name = key;
   }
 
+  /// Exports state containing [page.visitCount] for every page in a Map of [pages].
   Map<String, dynamic> exportState() {
     var pageMapState = new Map<String, dynamic>();
     pages.forEach((name, page) {
@@ -86,6 +98,8 @@ class EgbScripterPageMap {
     return pageMapState;
   }
 
+  /// Imports state from a Map [pageMapState].
+  /// Sets [page.visitCount] for every page in a Map of [pages] from a given Map.
   void importState(Map<String, dynamic> pageMapState) {
     pageMapState.forEach((name, map) {
       if (pages.containsKey(name)) {
