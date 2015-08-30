6d99b5e1bb5e99dcf45fea09afe6eebee36933c1
Filip Hracek <filip.hracek@gmail.com>
2015-08-30 13:39:03 +0200

    Update unit_test_run.sh

--- tool/unit_test_run.sh
+++ tool/unit_test_run.sh
@@ -1,11 +1,11 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-/Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../test/builder_test.dart && /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../test/scripter_test.dart && /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $DIR/../test/vars_generator_test.dart
+dart --enable_type_checks --enable_asserts $DIR/tester.dart
 if [ $? -gt 0 ]; then
   exit
 fi
 for f in $( ls $DIR/../books/libraries/*_test.dart ); do
   echo $f
-  /Applications/dart/dart-sdk/bin/dart --enable_type_checks --enable_asserts $f
+  dart --enable_type_checks --enable_asserts $f
   if [ $? -gt 0 ]; then
     exit
   fi
