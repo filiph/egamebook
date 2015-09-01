5dfca6fd21cc60d4133574124baa3d792d71d9fb
Filip Hracek <filip.hracek@gmail.com>
2015-09-01 16:53:19 +0200

    Update import for game library unit tests
    From unittest to test

--- books/libraries/storyline_test.dart
+++ books/libraries/storyline_test.dart
@@ -1,4 +1,4 @@
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'storyline.dart';
 
 void main() {
--- books/libraries/timeline_test.dart
+++ books/libraries/timeline_test.dart
@@ -1,4 +1,4 @@
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:egamebook/scripter.dart';
 import 'timeline.dart';
 
