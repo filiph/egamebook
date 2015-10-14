d423b053366257b19f79d84bfc8688f7b9277c91
Jana Moudrá <jana.moudra@gmail.com>
2015-10-14 10:35:55 +0200

    Fixed dartdocgen to new dartdoc. After serving, the generated file is opened in the browser.

--- bin/docgen.dart
+++ bin/docgen.dart
@@ -2,25 +2,16 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 import 'package:args/args.dart';
 
-/// Script for documentation generator - dartdocgen.
-/// This script finds all .dart files inside lib/ folder (including src/)
-/// and generates documentation from it.
+/// Script for documentation generator - dartdoc.
+/// This script finds generates documentation for this library and opens it
+/// in a browser.
+/// More info about dartdoc params in
+/// [documentation](https://github.com/dart-lang/dartdoc).
 
-/// Files which are ignored by docgen.
-final List ignoredFileNames = [
-  "reference_scripter_impl.dart",
-  "points_counter.dart", //contains part of
-  "scripter_page.dart" //contains part of
-];
+final String OUTPUT_PATH = "docs${path.separator}dev";
 
-/// More info about dartdocgen params in
-/// [documentation](https://www.dartlang.org/tools/dartdocgen/).
-final List docgenParams = [
-  "-v",
-  "--no-include-dependent-packages",
-  "--no-include-sdk",
-  "--start-page=egamebook",
-  "--out=docs/dev"
+final List params = [
+  "--output=$OUTPUT_PATH"
 ];
 
 /// If we want to serve the output
@@ -29,7 +20,7 @@ bool serve = false;
 void main(List<String> args) {
   ArgParser parser = new ArgParser()
     ..addFlag("serve", negatable: false,
-        help: "After generating, start the dartdoc-viewer on localhost:8080.")
+        help: "After generating, open docs in the browser.")
     ..addFlag("help", abbr: "h", negatable: false, help: "Display help.");
   ArgResults results = parser.parse(args);
 
@@ -42,53 +33,42 @@ void main(List<String> args) {
     return;
   }
 
-  String pathToScript = Platform.script.toFilePath();
-  Directory parent = new Directory(path.dirname(pathToScript)).parent;
+  Directory parent = new Directory(
+      path.dirname(Platform.script.toFilePath())).parent;
   Directory lib = new Directory(path.join(parent.path, "lib"));
-  Directory docViewer = new Directory(path.join(parent.path, "dartdoc-viewer"));
 
   if (!lib.existsSync()) {
     print("The folder lib/ doesn't exist. Canceling generation.");
     return;
   }
 
-  // dartdoc-viewer has to be deleted before generation
-  if (docViewer.existsSync()) {
-    docViewer.deleteSync(recursive: true);
-  }
-
-  List fileNames = [];
-  lib.list(recursive: true, followLinks: false)
-    .listen((FileSystemEntity entity) {
-    String fileName = path.basename(entity.path);
-
-    if (path.extension(entity.path) == ".dart" &&
-        !ignoredFileNames.contains(fileName)) {
-      fileNames.add(entity.path);
-    }
-  }, onDone: () => _runDocgen(fileNames));
+  _runDartDoc();
 }
 
-/// Start dartdocgen with parameters.
-void _runDocgen(List fileNames) {
-  docgenParams.add(_getPackageRoot());
-  if (serve) {
-    docgenParams.add("--serve");
-  }
+/// Start dartdoc with parameters.
+_runDartDoc() async {
+   ProcessResult results = await Process.run('dartdoc', params, runInShell: true);
+   print(results.stdout);
+   print(results.stderr);
 
-  List params = new List.from(docgenParams)
-      ..addAll(fileNames);
-
-  Process.start('dartdocgen', params, runInShell: true).then((process) {
-    stdout.addStream(process.stdout);
-    stderr.addStream(process.stderr);
-  });
+   //Opens a file in browser if serve is true
+   File indexFile = new File.fromUri(
+       Platform.script.resolve("..${path.separator}$OUTPUT_PATH${path.separator}index.html"));
+   if (serve && indexFile.existsSync()) {
+     _serveDoc("${indexFile.absolute.path}");
+   }
 }
 
-/// Get correct package root according to the platform.
-String _getPackageRoot() {
-  if (Platform.isWindows) {
-    return "--package-root=.\\packages";
+/// Serves documentation in the web browser.
+_serveDoc(String destination) async {
+  String command;
+  if (Platform.isMacOS) {
+    command = "open";
+  } else if (Platform.isWindows) {
+    command = "start";
+  } else if (Platform.isLinux) {
+    command = "xdg-open"; //should work on the most versions of Linux
   }
-  return "--package-root=./packages";
+
+  await Process.run(command, [destination], runInShell: true);
 }
