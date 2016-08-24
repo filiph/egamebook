7690c5689d07062ec9d765452dd1a20ac88eaee8
Filip Hracek <filip.hracek@gmail.com>
2016-08-24 12:14:27 -0700

    Improve splash screen

--- lib/src/fight/pass.dart
+++ lib/src/fight/pass.dart
@@ -1,5 +1,5 @@
 import 'package:stranded/action.dart';
 
-var pass = new ClosureActorAction("Pass.", (_, __) => true, (a, w, s) {
+var pass = new ClosureActorAction("Stand firm.", (_, __) => true, (a, w, s) {
   return "${a.name} passes the opportunity";
 }, (_, __, ___) {}, 1.0);
