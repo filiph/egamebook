50f991a648e7f4edc5725f0a6acffcf9a872653c
Filip Hracek <filip.hracek@gmail.com>
2017-01-04 21:55:27 -0800

    Fix config-specific import order
    The wrong order had the effect of blocking UI thread when dart2js’d.

--- lib/generic_animation_frame/animation_frame.dart
+++ lib/generic_animation_frame/animation_frame.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'src/animation_frame_interface.dart'
-    if (dart.library.io) 'src/animation_frame_io.dart'
-    if (dart.library.html) 'src/animation_frame_html.dart';
+    if (dart.library.html) 'src/animation_frame_html.dart'
+    if (dart.library.io) 'src/animation_frame_io.dart';
 
 /// On the web, returns a Future that completes just before the window is about
 /// to repaint so the user can draw an animation frame. Outside of the web,
