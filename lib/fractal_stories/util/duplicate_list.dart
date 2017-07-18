cf9c4a817ec9fefa7c4a37ad610c78bb04a3562a
Filip Hracek <filip.hracek@gmail.com>
2017-07-18 12:17:37 -0700

    Move from old method type annotation to new

--- lib/fractal_stories/util/duplicate_list.dart
+++ lib/fractal_stories/util/duplicate_list.dart
@@ -1,10 +1,10 @@
 library duplicate_list;
 
 /// Makes a deep copy of the [original] set, using [clone] for each member.
-List/*<T>*/ duplicateList/*<T>*/(
-    List/*<T>*/ original, dynamic/*=T*/ clone(/*=T*/ object)) {
-  List/*<T>*/ result = new List/*<T>*/();
-  for (dynamic/*=T*/ member in original) {
+List<T> duplicateList<T>(
+    List<T> original, T clone(T object)) {
+  List<T> result = new List<T>();
+  for (T member in original) {
     result.add(clone(member));
   }
   return result;
--- lib/fractal_stories/util/duplicate_set.dart
+++ lib/fractal_stories/util/duplicate_set.dart
@@ -1,10 +1,10 @@
 library duplicate_set;
 
 /// Makes a deep copy of the [original] set, using [clone] for each member.
-Set/*<T>*/ duplicateSet/*<T>*/(
-    Set/*<T>*/ original, dynamic/*=T*/ clone(/*=T*/ object)) {
-  Set/*<T>*/ result = new Set/*<T>*/();
-  for (dynamic/*=T*/ member in original) {
+Set<T> duplicateSet<T>(
+    Set<T> original, T clone(T object)) {
+  Set<T> result = new Set<T>();
+  for (T member in original) {
     result.add(clone(member));
   }
   return result;
