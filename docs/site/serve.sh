3b04b718d75f4377083ba713f49f7eabf7d2ec1c
Filip Hracek <filip.hracek@gmail.com>
2017-09-22 20:22:09 -0700

    Set up serving with superstatic

--- docs/site/serve.sh
+++ docs/site/serve.sh
@@ -1,1 +1,14 @@
-jekyll serve --config _config.yml,_config_dev.yml
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+jekyll build --config _config.yml,_config_dev.yml --incremental --watch &
+j_pid=$!
+
+superstatic --port 5000 &
+f_pid=$!
+
+echo "catched PIDs: $j_pid, $f_pid"
+
+trap "{ kill $j_pid; kill $f_pid; exit 0;}" SIGINT
+wait
