388e8d4fe9591357ae29243a915ae0bdcf127cd9
Filip Hracek <filip.hracek@gmail.com>
2018-03-18 19:06:58 -0700

    Remove unused state from EdgeheadGlobal

--- lib/edgehead_global.dart
+++ lib/edgehead_global.dart
@@ -18,25 +18,4 @@ abstract class EdgeheadGlobalState
       _$EdgeheadGlobalState;
 
   EdgeheadGlobalState._();
-
-  int get bloodrockFollowers;
-
-  int get brianaQuoteIndex;
-
-  bool get hasKegOfBeer;
-}
-
-abstract class EdgeheadGlobalStateBuilder
-    implements Builder<EdgeheadGlobalState, EdgeheadGlobalStateBuilder> {
-  @virtual
-  bool hasKegOfBeer = false;
-
-  @virtual
-  int bloodrockFollowers = 0;
-
-  @virtual
-  int brianaQuoteIndex = 0;
-
-  factory EdgeheadGlobalStateBuilder() = _$EdgeheadGlobalStateBuilder;
-  EdgeheadGlobalStateBuilder._();
 }
--- lib/edgehead_global.g.dart
+++ lib/edgehead_global.g.dart
@@ -30,157 +30,53 @@ class _$EdgeheadGlobalStateSerializer
   @override
   Iterable serialize(Serializers serializers, EdgeheadGlobalState object,
       {FullType specifiedType: FullType.unspecified}) {
-    final result = <Object>[
-      'bloodrockFollowers',
-      serializers.serialize(object.bloodrockFollowers,
-          specifiedType: const FullType(int)),
-      'brianaQuoteIndex',
-      serializers.serialize(object.brianaQuoteIndex,
-          specifiedType: const FullType(int)),
-      'hasKegOfBeer',
-      serializers.serialize(object.hasKegOfBeer,
-          specifiedType: const FullType(bool)),
-    ];
-
-    return result;
+    return <Object>[];
   }
 
   @override
   EdgeheadGlobalState deserialize(Serializers serializers, Iterable serialized,
       {FullType specifiedType: FullType.unspecified}) {
-    final result = new EdgeheadGlobalStateBuilder();
-
-    final iterator = serialized.iterator;
-    while (iterator.moveNext()) {
-      final key = iterator.current as String;
-      iterator.moveNext();
-      final dynamic value = iterator.current;
-      switch (key) {
-        case 'bloodrockFollowers':
-          result.bloodrockFollowers = serializers.deserialize(value,
-              specifiedType: const FullType(int)) as int;
-          break;
-        case 'brianaQuoteIndex':
-          result.brianaQuoteIndex = serializers.deserialize(value,
-              specifiedType: const FullType(int)) as int;
-          break;
-        case 'hasKegOfBeer':
-          result.hasKegOfBeer = serializers.deserialize(value,
-              specifiedType: const FullType(bool)) as bool;
-          break;
-      }
-    }
-
-    return result.build();
+    return new EdgeheadGlobalStateBuilder().build();
   }
 }
 
 class _$EdgeheadGlobalState extends EdgeheadGlobalState {
-  @override
-  final int bloodrockFollowers;
-  @override
-  final int brianaQuoteIndex;
-  @override
-  final bool hasKegOfBeer;
-
   factory _$EdgeheadGlobalState([void updates(EdgeheadGlobalStateBuilder b)]) =>
-      (new EdgeheadGlobalStateBuilder()..update(updates)).build()
-          as _$EdgeheadGlobalState;
+      (new EdgeheadGlobalStateBuilder()..update(updates)).build();
 
-  _$EdgeheadGlobalState._(
-      {this.bloodrockFollowers, this.brianaQuoteIndex, this.hasKegOfBeer})
-      : super._() {
-    if (bloodrockFollowers == null)
-      throw new ArgumentError.notNull('bloodrockFollowers');
-    if (brianaQuoteIndex == null)
-      throw new ArgumentError.notNull('brianaQuoteIndex');
-    if (hasKegOfBeer == null) throw new ArgumentError.notNull('hasKegOfBeer');
-  }
+  _$EdgeheadGlobalState._() : super._();
 
   @override
   EdgeheadGlobalState rebuild(void updates(EdgeheadGlobalStateBuilder b)) =>
       (toBuilder()..update(updates)).build();
 
   @override
-  _$EdgeheadGlobalStateBuilder toBuilder() =>
-      new _$EdgeheadGlobalStateBuilder()..replace(this);
+  EdgeheadGlobalStateBuilder toBuilder() =>
+      new EdgeheadGlobalStateBuilder()..replace(this);
 
   @override
   bool operator ==(dynamic other) {
     if (identical(other, this)) return true;
     if (other is! EdgeheadGlobalState) return false;
-    return bloodrockFollowers == other.bloodrockFollowers &&
-        brianaQuoteIndex == other.brianaQuoteIndex &&
-        hasKegOfBeer == other.hasKegOfBeer;
+    return true;
   }
 
   @override
   int get hashCode {
-    return $jf($jc(
-        $jc($jc(0, bloodrockFollowers.hashCode), brianaQuoteIndex.hashCode),
-        hasKegOfBeer.hashCode));
+    return 712033461;
   }
 
   @override
   String toString() {
-    return (newBuiltValueToStringHelper('EdgeheadGlobalState')
-          ..add('bloodrockFollowers', bloodrockFollowers)
-          ..add('brianaQuoteIndex', brianaQuoteIndex)
-          ..add('hasKegOfBeer', hasKegOfBeer))
-        .toString();
+    return newBuiltValueToStringHelper('EdgeheadGlobalState').toString();
   }
 }
 
-class _$EdgeheadGlobalStateBuilder extends EdgeheadGlobalStateBuilder {
+class EdgeheadGlobalStateBuilder
+    implements Builder<EdgeheadGlobalState, EdgeheadGlobalStateBuilder> {
   _$EdgeheadGlobalState _$v;
 
-  @override
-  int get bloodrockFollowers {
-    _$this;
-    return super.bloodrockFollowers;
-  }
-
-  @override
-  set bloodrockFollowers(int bloodrockFollowers) {
-    _$this;
-    super.bloodrockFollowers = bloodrockFollowers;
-  }
-
-  @override
-  int get brianaQuoteIndex {
-    _$this;
-    return super.brianaQuoteIndex;
-  }
-
-  @override
-  set brianaQuoteIndex(int brianaQuoteIndex) {
-    _$this;
-    super.brianaQuoteIndex = brianaQuoteIndex;
-  }
-
-  @override
-  bool get hasKegOfBeer {
-    _$this;
-    return super.hasKegOfBeer;
-  }
-
-  @override
-  set hasKegOfBeer(bool hasKegOfBeer) {
-    _$this;
-    super.hasKegOfBeer = hasKegOfBeer;
-  }
-
-  _$EdgeheadGlobalStateBuilder() : super._();
-
-  EdgeheadGlobalStateBuilder get _$this {
-    if (_$v != null) {
-      super.bloodrockFollowers = _$v.bloodrockFollowers;
-      super.brianaQuoteIndex = _$v.brianaQuoteIndex;
-      super.hasKegOfBeer = _$v.hasKegOfBeer;
-      _$v = null;
-    }
-    return this;
-  }
+  EdgeheadGlobalStateBuilder();
 
   @override
   void replace(EdgeheadGlobalState other) {
@@ -195,11 +91,7 @@ class _$EdgeheadGlobalStateBuilder extends EdgeheadGlobalStateBuilder {
 
   @override
   _$EdgeheadGlobalState build() {
-    final _$result = _$v ??
-        new _$EdgeheadGlobalState._(
-            bloodrockFollowers: bloodrockFollowers,
-            brianaQuoteIndex: brianaQuoteIndex,
-            hasKegOfBeer: hasKegOfBeer);
+    final _$result = _$v ?? new _$EdgeheadGlobalState._();
     replace(_$result);
     return _$result;
   }
