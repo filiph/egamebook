a49b49e7fcc393ba8b3d19789d7305746fe6e5b2
Filip Hracek <filip.hracek@gmail.com>
2018-08-15 21:19:16 -0700

    Setup mono_repo
    Use the mono_repo tool to build and test everything in one place.

---
+++ tool/travis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Created with https://github.com/dart-lang/mono_repo
+
+if [ -z "$PKG" ]; then
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+  exit 1
+fi
+
+if [ "$#" == "0" ]; then
+  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
+  exit 1
+fi
+
+pushd $PKG
+pub upgrade || exit $?
+
+EXIT_CODE=0
+
+while (( "$#" )); do
+  TASK=$1
+  case $TASK in
+  command_0) echo
+    echo -e '\033[1mTASK: command_0\033[22m'
+    echo -e 'pub run build_runner build --delete-conflicting-outputs'
+    pub run build_runner build --delete-conflicting-outputs || EXIT_CODE=$?
+    ;;
+  command_1) echo
+    echo -e '\033[1mTASK: command_1\033[22m'
+    echo -e 'pub run test'
+    pub run test || EXIT_CODE=$?
+    ;;
+  command_2) echo
+    echo -e '\033[1mTASK: command_2\033[22m'
+    echo -e 'pub run test -t long-running --run-skipped'
+    pub run test -t long-running --run-skipped || EXIT_CODE=$?
+    ;;
+  dartanalyzer) echo
+    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+    ;;
+  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    EXIT_CODE=1
+    ;;
+  esac
+
+  shift
+done
+
+exit $EXIT_CODE
