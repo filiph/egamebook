894c16af906349fc26e863243f5b5f611731426f
Filip Hracek <filip.hracek@gmail.com>
2018-09-16 10:27:50 -0700

    Apply dartfmt

--- edgehead/lib/src/fight/actions/throw_thrusting_weapon.dart
+++ edgehead/lib/src/fight/actions/throw_thrusting_weapon.dart
@@ -161,9 +161,11 @@ class ThrowThrustingWeapon extends EnemyTargetAction {
   void _moveSwordToGround(WorldStateBuilder w, Actor a, Item sword) {
     final fightSituation =
         w.getSituationByName<FightSituation>(FightSituation.className);
-    w.updateActorById(a.id, (b) => b
-        ..inventory.remove(sword)
-        ..inventory.goBarehanded(a.anatomy));
+    w.updateActorById(
+        a.id,
+        (b) => b
+          ..inventory.remove(sword)
+          ..inventory.goBarehanded(a.anatomy));
     w.replaceSituationById(fightSituation.id,
         fightSituation.rebuild((b) => b..droppedItems.add(sword)));
   }
