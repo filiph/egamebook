276c2558cb0022183bdb05a5a6586a720ef57aa5
Filip Hracek <filip.hracek@gmail.com>
2019-03-22 17:09:54 -0700

    Remove redundant parens

--- edgehead/lib/src/fight/actions/sweep_feet.dart
+++ edgehead/lib/src/fight/actions/sweep_feet.dart
@@ -100,7 +100,7 @@ class SweepFeet extends EnemyTargetAction with ComplexCommandPath<Actor> {
 
   @override
   bool isApplicable(Actor a, Simulation sim, WorldState world, Actor enemy) =>
-      (a.pose >= Pose.offBalance) &&
+      a.pose >= Pose.offBalance &&
       !enemy.isOnGround &&
       !a.anatomy.isBlind &&
       enemy.pose <= Pose.extended;
