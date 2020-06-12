0dcef2a8f0a0bc0733a249db9be2c197d78c772b
Filip Hracek <filip.hracek@gmail.com>
2020-06-12 11:31:57 -0700

    Prevent Karl from growling when dead

--- edgehead/lib/director/environmental/karl_heard.dart
+++ edgehead/lib/director/environmental/karl_heard.dart
@@ -4,7 +4,9 @@ final _karlHeardFirstTime = Rule(_id++, 1, true, (ApplicabilityContext c) {
   // Only heard from within the Pyramid.
   return c.playerDistanceTo('gods_lair') < 45 &&
       // Must be way below God's lair.
-      c.playerParentRoom.positionY > 50;
+      c.playerParentRoom.positionY > 50 &&
+      // Obviously, Karl must still be alive.
+      !c.hasHappened(evKarlKilled);
 }, (ActionContext c) {
   final Storyline s = c.outputStoryline;
   s.addParagraph();
