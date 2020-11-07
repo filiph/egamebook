8628140dccbd9dc45d9a4fb2f3f981ce1ce2f027
Filip Hracek <filip.hracek@gmail.com>
2020-11-06 22:32:53 -0800

    Remove redundant hints

--- edgehead/lib/writers_input.compiled.dart
+++ edgehead/lib/writers_input.compiled.dart
@@ -2928,7 +2928,7 @@ final Room conet = Room(
     firstMapName: 'Some Mechanism',
     hint: 'A room filled with a large device.',
     firstHint:
-        'Somethings is making a quiet chafing sounds similar. Reminds me of millstones.',
+        'Somethings is making a quiet chafing sounds. Reminds me of millstones.',
     afterMonstersCleared: (ActionContext c) {
       final WorldState originalWorld = c.world;
       final Simulation sim = c.simulation;
@@ -2980,7 +2980,7 @@ final Room conetAfterClearing = Room(
     firstMapName: 'Some Mechanism',
     hint: 'A room filled with a large device.',
     firstHint:
-        'Somethings is making a quiet chafing sounds similar. Reminds me of millstones.',
+        'Somethings is making a quiet chafing sounds. Reminds me of millstones.',
     afterMonstersCleared: (ActionContext c) {
       final WorldState originalWorld = c.world;
       final Simulation sim = c.simulation;
@@ -6668,10 +6668,7 @@ final Room jungleEntrance = Room('jungle_entrance', (ActionContext c) {
     positionX: 20,
     positionY: 68,
     mapName: 'Jungle',
-    firstMapName: 'Jungle',
     hint:
-        'This is where the interior of the Pyramid opens into a large crater, covered in vegetation.',
-    firstHint:
         'This is where the interior of the Pyramid opens into a large crater, covered in vegetation.');
 final Room jungleEntranceMuddyFootprints = Room(
     'jungle_entrance_muddy_footprints',
@@ -6713,10 +6710,7 @@ final Room jungleEntranceMuddyFootprints = Room(
     positionX: 20,
     positionY: 68,
     mapName: 'Jungle',
-    firstMapName: 'Jungle',
     hint:
-        'This is where the interior of the Pyramid opens into a large crater, covered in vegetation.',
-    firstHint:
         'This is where the interior of the Pyramid opens into a large crater, covered in vegetation.');
 final Approach deathlessVillageFromDragonEggRoom =
     Approach('dragon_egg_room', 'deathless_village', '', null);
@@ -8203,10 +8197,7 @@ final Room knightsHqMain = Room('knights_hq_main', (ActionContext c) {
     positionX: 37,
     positionY: 65,
     mapName: 'Command Post',
-    firstMapName: 'Command Post',
     hint:
-        'This is the original center of operations of the Knights of San Francisco.',
-    firstHint:
         'This is the original center of operations of the Knights of San Francisco.');
 final talkToMiguelAboutDesertingInk = InkAst([
   InkParagraphNode((ActionContext c) {
@@ -8431,10 +8422,7 @@ final Room knightsHqCaravanDeparture = Room(
     positionX: 37,
     positionY: 65,
     mapName: 'Command Post',
-    firstMapName: 'Command Post',
     hint:
-        'This is the original center of operations of the Knights of San Francisco.',
-    firstHint:
         'This is the original center of operations of the Knights of San Francisco.');
 final Room knightsHqOrcOffensive = Room(
     'knights_hq_orc_offensive',
@@ -8478,10 +8466,7 @@ final Room knightsHqOrcOffensive = Room(
     positionX: 37,
     positionY: 65,
     mapName: 'Command Post',
-    firstMapName: 'Command Post',
     hint:
-        'This is the original center of operations of the Knights of San Francisco.',
-    firstHint:
         'This is the original center of operations of the Knights of San Francisco.');
 final Room knightsHqQuake2 = Room(
     'knights_hq_quake2',
@@ -8526,10 +8511,7 @@ final Room knightsHqQuake2 = Room(
     positionX: 37,
     positionY: 65,
     mapName: 'Command Post',
-    firstMapName: 'Command Post',
     hint:
-        'This is the original center of operations of the Knights of San Francisco.',
-    firstHint:
         'This is the original center of operations of the Knights of San Francisco.');
 final Room knightsHqQuake3 = Room(
     'knights_hq_quake3',
@@ -8572,10 +8554,7 @@ final Room knightsHqQuake3 = Room(
     positionX: 37,
     positionY: 65,
     mapName: 'Command Post',
-    firstMapName: 'Command Post',
     hint:
-        'This is the original center of operations of the Knights of San Francisco.',
-    firstHint:
         'This is the original center of operations of the Knights of San Francisco.');
 final Approach elevator12FromElevator28 =
     Approach('elevator_28', 'elevator_12', '', (ActionContext c) {
