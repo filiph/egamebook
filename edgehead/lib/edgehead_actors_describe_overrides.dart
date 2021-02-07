b789a57abdd48a82b2643ef50f5c605e95869fd7
Filip Hracek <filip.hracek@gmail.com>
2021-02-06 20:42:43 -0800

    Don’t show Leroy before we get to know about him
    Also, extract writer’s context extensions into `writers_context_extensions.dart` so we can use them in more places (without cyclical dependencies).

--- edgehead/lib/edgehead_actors_describe_overrides.dart
+++ edgehead/lib/edgehead_actors_describe_overrides.dart
@@ -1,41 +1,52 @@
 import 'package:edgehead/edgehead_ids.dart';
 import 'package:edgehead/fractal_stories/context.dart';
 import 'package:edgehead/src/room_roaming/room_roaming_situation.dart';
+import 'package:edgehead/writers_context_extensions.dart';
 
 final Map<int, bool Function(ActionContext)> edgeheadActorDescribeOverrides = {
   tamaraId: _tamaraOverrides,
   katId: _katOverrides,
   miguelId: _miguelOverrides,
+  leroyId: _leroyOverrides,
 };
 
-bool _isFirstVisitToPyramidEntrance(ActionContext context) {
+bool _isFirstVisitToPyramidEntrance(ActionContext c) {
   final roomName =
-      (context.world.currentSituation as RoomRoamingSituation).currentRoomName;
+      (c.world.currentSituation as RoomRoamingSituation).currentRoomName;
   if (roomName != 'pyramid_entrance') return false;
-  final room = context.simulation.getRoomByName(roomName);
-  final hasVisitedAnyVariant = context.world.visitHistory
-      .query(context.actor, room, includeVariants: true)
+  final room = c.simulation.getRoomByName(roomName);
+  final hasVisitedAnyVariant = c.world.visitHistory
+      .query(c.actor, room, includeVariants: true)
       .hasHappened;
   return !hasVisitedAnyVariant;
 }
 
-bool _katOverrides(ActionContext context) {
-  if (_isFirstVisitToPyramidEntrance(context)) {
+bool _katOverrides(ActionContext c) {
+  if (_isFirstVisitToPyramidEntrance(c)) {
     return true;
   }
 
   return false;
 }
 
-bool _miguelOverrides(ActionContext context) {
-  if (_isFirstVisitToPyramidEntrance(context)) {
+bool _leroyOverrides(ActionContext c) {
+  if (!c.knows(kbLeroy)) {
+    // Don't show Leroy before we actually learn about him.
     return true;
   }
 
   return false;
 }
 
-bool _tamaraOverrides(ActionContext context) {
+bool _miguelOverrides(ActionContext c) {
+  if (_isFirstVisitToPyramidEntrance(c)) {
+    return true;
+  }
+
+  return false;
+}
+
+bool _tamaraOverrides(ActionContext c) {
   // Never describe Tamara's presence.
   return true;
 }
